aed522c30453276791776d1c3c615c32ace19bb9
Eddie <edvardappelberg25@gmail.com>
2026-03-22 15:16:50 +0100

    david9

--- manuals.js
+++ manuals.js
@@ -4,5 +4,5 @@
 
 SEK = 50433
 EUR = 5422
-const DONATION_TOTAL  = SEK + EUR*11;       // current total in SEK — update this each evening
+const DONATION_TOTAL  = int(SEK + EUR*11);       // current total in SEK — update this each evening
 const DONATION_GOAL   = 100000;  // your goal in SEK
