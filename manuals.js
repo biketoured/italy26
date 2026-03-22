9e8d4b6be4450587caad9f52435f10e4a21b25d6
Eddie <edvardappelberg25@gmail.com>
2026-03-22 22:54:33 +0100

    friidrottvm4

--- manuals.js
+++ manuals.js
@@ -6,3 +6,11 @@ const SEK = 8433;
 const EUR = 5422;
 const DONATION_TOTAL = Math.round(SEK + (EUR*11));  // current total in SEK
 const DONATION_GOAL  = 100000;                       // goal in SEK
+
+
+// Write something personal to me
+// Blogginlägg
+// Översätta donate.html
+// Checka storlekar
+// Ikoner
+// Utforska betalningssätt
